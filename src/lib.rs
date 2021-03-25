0d869bdb11ca6e1d9e9557dad03fc897b0602199
Makoto Shimazu <makoto.shimaz@gmail.com>
2021-03-25 20:58:01 +0900

    Add descriptions for modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 //! Rust port of [pwntools](https://github.com/Gallopsled/pwntools)
 //!
 
+/// Give you an easy interaction with an executable launched as a child process.
 pub mod process;
+
+/// Retrive info from your elf binary.
 pub mod pwn;
