76ab6e4bdc0e3f242d4323e63332992bfac6aa41
Makoto Shimazu <makoto.shimaz@gmail.com>
2021-03-25 21:09:03 +0900

    Add more documents in pwn.rs

--- src/pwn.rs
+++ src/pwn.rs
@@ -16,53 +16,55 @@ use elf_utilities::{
     file, section,
     section::{Contents64, Section64},
 };
-use num_derive::{FromPrimitive, ToPrimitive};
-use num_traits::FromPrimitive;
 
-#[derive(FromPrimitive, ToPrimitive, PartialEq, Debug)]
-pub enum StBind {
-    Local = 0,
-    Global = 1,
-    Weak = 2,
-    LoProc = 13,
-    HiProc = 15,
-}
+// use num_derive::{FromPrimitive, ToPrimitive};
+// use num_traits::FromPrimitive;
 
-#[derive(FromPrimitive, ToPrimitive, PartialEq, Debug)]
-pub enum StType {
-    NoType = 0,
-    Object = 1,
-    Func = 2,
-    Section = 3,
-    File = 4,
-    LoProc = 13,
-    HiProc = 15,
-}
+// #[derive(FromPrimitive, ToPrimitive, PartialEq, Debug)]
+// pub enum StBind {
+//     Local = 0,
+//     Global = 1,
+//     Weak = 2,
+//     LoProc = 13,
+//     HiProc = 15,
+// }
 
-pub fn read_st_info(st_info: u8) -> (StType, StBind) {
-    (
-        StType::from_u8(st_info & 0xF).expect(&format!("StType: {}", st_info & 0xF)),
-        StBind::from_u8((st_info >> 4) & 0xF).expect(&format!("StBind: {}", st_info >> 4 & 0xF)),
-    )
-}
+// #[derive(FromPrimitive, ToPrimitive, PartialEq, Debug)]
+// pub enum StType {
+//     NoType = 0,
+//     Object = 1,
+//     Func = 2,
+//     Section = 3,
+//     File = 4,
+//     LoProc = 13,
+//     HiProc = 15,
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// fn read_st_info(st_info: u8) -> (StType, StBind) {
+//     (
+//         StType::from_u8(st_info & 0xF).expect(&format!("StType: {}", st_info & 0xF)),
+//         StBind::from_u8((st_info >> 4) & 0xF).expect(&format!("StBind: {}", st_info >> 4 & 0xF)),
+//     )
+// }
 
-    #[test]
-    fn good_st_info() {
-        assert_eq!(read_st_info(0x12), (StType::Func, StBind::Global));
-    }
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    #[should_panic(expected = "StBind: 3")]
-    fn bad_st_info() {
-        read_st_info(0x32);
-    }
-}
+//     #[test]
+//     fn good_st_info() {
+//         assert_eq!(read_st_info(0x12), (StType::Func, StBind::Global));
+//     }
+
+//     #[test]
+//     #[should_panic(expected = "StBind: 3")]
+//     fn bad_st_info() {
+//         read_st_info(0x32);
+//     }
+// }
 
 pub struct Pwn {
+    /// The ELF binary being processed.
     pub elf: file::ELF64,
 }
 
@@ -72,6 +74,7 @@ impl Pwn {
         Self { elf }
     }
 
+    /// Returns the address of the symbol.
     pub fn symbol(&self, name: &str) -> Option<u64> {
         let mut result = Vec::new();
         for section in &self.elf.sections {
@@ -93,6 +96,7 @@ impl Pwn {
         result.first().copied()
     }
 
+    /// Search the symbol's address in the Global Offset Table.
     pub fn got(&self, name: &str) -> Option<u64> {
         let rela_plt = self.get_section(".rela.plt")?;
 
@@ -119,6 +123,7 @@ impl Pwn {
         }
     }
 
+    /// The beginning of the bss section.
     pub fn bss(&self) -> Option<u64> {
         let bss = self.get_section(".bss")?;
         Some(bss.header.sh_addr)
