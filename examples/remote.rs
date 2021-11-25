643c5fdc641a590e28abc79af84ca0c575041173
S-Nakamur-a <25317025+S-Nakamur-a@users.noreply.github.com>
2021-11-25 21:47:50 +0900

    remove mut

--- examples/remote.rs
+++ examples/remote.rs
@@ -2,7 +2,7 @@ use pwntools::{connection::Connection, connection::Remote};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let mut remo = Remote::new("127.0.0.1:12345").await?;
+    let remo = Remote::new("127.0.0.1:12345").await?;
     remo.interactive().await?;
     Ok(())
 }
